9112cf08d12b8c3889b344ad133e557a732e6d83
David Carew <david@dcarew.com>
2026-02-10 19:52:50 -0800

    Add field for image attachments to Post

--- post.py
+++ post.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from pillow import Image
 
 @dataclass()
 class Post:
     """Basic representation of an item to print. This is potentially coming from multiple sources with different formats, so only has a lowest common denominator of data"""
     user_handle: str
     body: str
+    attachments: list[Image]
 
