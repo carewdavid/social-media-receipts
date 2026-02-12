3d9e56b419571f666d320b0dd7ad7b234de03bd6
David Carew <david@dcarew.com>
2026-02-11 18:51:14 -0800

    Read config file

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import argparse
+import tomllib
 
 def main():
     argument_parser = argparse.ArgumentParser()
     argument_parser.add_argument("-p", "--printer", help="Path to line printer", default="/dev/usb/lp0")
     argument_parser.add_argument("-c", "--config", help="Path to config file", default="receipt.conf")
     args = argument_parser.parse_args()
-    print(args)
+    with open(args.config, "rb") as config_file:
+        config = tomllib.load(config_file)
+        printer = Printer(args.printer, config['printer'])
+        feeds = []
+        for feed in config['feeds'].items():
+            feeds.append(feed)
+
 
 
 if __name__ == "__main__":
