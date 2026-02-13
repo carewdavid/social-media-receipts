2e058ccdf9a7e9490e7c002247e2650e5c978f35
David Carew <david@dcarew.com>
2026-02-12 21:11:20 -0800

    Add print method to Printer

--- printer.py
+++ printer.py
@@ -1,4 +1,5 @@
 from typing import BinaryIO, Dict
+from post import Post
 
 
 class Printer:
@@ -11,3 +12,9 @@ class Printer:
         #TODO make sure things get cleaned up properly. Possibly by making this class a context manager.
         #In the meantime, we tell ourselves its okay because this file will be open for the lifetime of the program. We can't do anything without it
         self.file_handle = open(path, "wb") 
+
+    def print_post(self, post: Post):
+        #TODO fancier formatting
+        self.file_handle.write(bytes(post.user_handle, "ascii"))
+        self.file_handle.write(bytes(post.body, "ascii"))
+        self.file_handle.flush()
